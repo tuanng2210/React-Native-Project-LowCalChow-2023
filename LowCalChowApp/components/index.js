8dc8b4a17620a0a10cc56603dc3979e3605f0dfc
Kendra Schall <ksscha005@odu.edu>
2023-09-19 18:51:03 -0400

    added some folders

---
+++ LowCalChowApp/components/index.js
@@ -0,0 +1,4 @@
+export {default as handshake} from './handshake/Handshake';
+export {default as loginPage} from './loginPage/LoginPage';
+export {default as patron_account_creation} from './patron_account_creation/PatronAccountCreation';
+export {default as patronHomepage} from './patronHomepage/PatronHomepage';
