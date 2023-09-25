787288d2767897becdd8da06b9a699e98e8a259c
Kendra Schall <ksscha005@odu.edu>
2023-09-25 13:18:56 -0400

    json lock

--- LowCalChowApp/components/index.js
+++ LowCalChowApp/components/index.js
@@ -1,4 +1,8 @@
-export {default as handshake} from './handshake/Handshake';
-export {default as loginPage} from './loginPage/LoginPage';
-export {default as patron_account_creation} from './patron_account_creation/PatronAccountCreation';
-export {default as patronHomepage} from './patronHomepage/PatronHomepage';
+/*export { default as handshake } from './handshake/Handshake';
+export { default as loginPage } from './loginPage/LoginPage';
+export { default as patron_account_creation } from './patron_account_creation/PatronAccountCreation';
+export { default as patronHomepage } from './patronHomepage/PatronHomepage';
+
+//import App from '../App';
+import '/.index.css';*/
+
--- LowCalChowApp/components/patronHomepage.jsx
+++ LowCalChowApp/components/patronHomepage.jsx
@@ -3,6 +3,24 @@ import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-function patronHomepage({navigation}) {}
- 
+  /*  function patronHomepage() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'LowCalChow Home' }}
+        />
+      </Stack.Navigator>
+    );
+  }
+
+    // header bar -Top left settings top right favorites
     
+
+    // main page recomended list 
+
+    // footer home, search, bookmark 
+}
+ 
+    */
