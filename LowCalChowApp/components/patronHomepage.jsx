76ba89f6755e7504f978a0474844f1124f9cdc46
Kendra Schall <91961398+kendraschall@users.noreply.github.com>
2023-09-29 14:21:43 -0400

    Update patronHomepage.jsx

--- LowCalChowApp/components/patronHomepage.jsx
+++ LowCalChowApp/components/patronHomepage.jsx
@@ -3,8 +3,12 @@ import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-  /*  function patronHomepage() {
-    return (
+ const Stack = createNativeStackNavigator();
+function patronHomepage() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login Page</Text>
+
       <Stack.Navigator>
         <Stack.Screen
           name="Home"
@@ -12,15 +16,29 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
           options={{ title: 'LowCalChow Home' }}
         />
       </Stack.Navigator>
-    );
-  }
-
-    // header bar -Top left settings top right favorites
-    
-
-    // main page recomended list 
-
-    // footer home, search, bookmark 
+    </View>
+  );
 }
- 
-    */
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+});
+
+
+// header bar -Top left settings top right favorites
+
+
+// main page recomended list 
+
+// footer home, search, bookmark 
+
+
+export default patronHomepage;
