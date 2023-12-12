0d3e9bf04d392cf5b0695047708d0c6f439e75d6
tuanng2210 <103005131+tuanng2210@users.noreply.github.com>
2023-12-12 16:30:49 -0500

    fix stuff

--- LowCalChowApp/components/individualFilteringOption.jsx
+++ LowCalChowApp/components/individualFilteringOption.jsx
@@ -1,6 +1,129 @@
+// import React, { useState, useEffect } from "react";
+// import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
+// import { BarChart } from "react-native-chart-kit";
+
+// const FilteredDataScreen = ({ route }) => {
+//   const { analyticsType, filterOptionId, accessToken } = route.params;
+//   const [data, setData] = useState(null);
+//   const [loading, setLoading] = useState(true);
+//   const [error, setError] = useState(null);
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const response = await fetch(
+//           `http://localhost:8000/analytics/${analyticsType}/${filterOptionId}/`,
+//           {
+//             headers: {
+//               Authorization: `Bearer ${accessToken}`,
+//               "Content-Type": "application/json",
+//             },
+//           }
+//         );
+
+//         if (!response.ok) {
+//           throw new Error(`HTTP error! Status: ${response.status}`);
+//         }
+
+//         const jsonData = await response.json();
+//         setData(jsonData);
+//         console.log(jsonData);
+//       } catch (error) {
+//         console.error("Fetch Error:", error);
+//         setError(error.message);
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
+
+//     fetchData();
+//   }, [analyticsType, filterOptionId, accessToken]);
+
+//   if (loading) {
+//     return (
+//       <View style={styles.container}>
+//         <ActivityIndicator size="large" color="#0000ff" />
+//       </View>
+//     );
+//   }
+
+//   if (error) {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.errorText}>Error: {error}</Text>
+//       </View>
+//     );
+//   }
+
+//   const chartData = {
+//     labels: ["Patron Profile", "Menu Item"],
+//     datasets: [
+//       {
+//         data: [data.number_of_patronProfile, data.number_of_menuItem],
+//       },
+//     ],
+//   };
+
+//   const chartConfig = {
+//     backgroundColor: "black",
+//     backgroundGradientFrom: "#392F5A",
+//     backgroundGradientTo: "#ffa726",
+//     decimalPlaces: 0,
+//     color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+//     barPercentage: 0.75,
+//   };
+
+//   console.log("ChartData:", chartData);
+
+//   return (
+//     <View style={styles.container}>
+//       {data && (
+//         <View style={styles.chartContainer}>
+//           <Text style={styles.chartTitle}>
+//             Number of Patron vs Num of Menu Items Containing the Tag
+//           </Text>
+//           <BarChart
+//             data={chartData}
+//             width={300}
+//             height={200}
+//             yAxisLabel="#"
+//             fromZero={true}
+//             chartConfig={chartConfig}
+//           />
+//         </View>
+//       )}
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: "center",
+//     justifyContent: "center",
+//   },
+//   screenTitle: {
+//     fontSize: 20,
+//     color: "#FFFFFF",
+//     marginBottom: 10,
+//   },
+//   chartContainer: {
+//     alignItems: "center",
+//   },
+//   chartTitle: {
+//     fontSize: 16,
+//     marginBottom: 10,
+//   },
+//   errorText: {
+//     fontSize: 16,
+//     color: "#FF0000",
+//   },
+// });
+
+// export default FilteredDataScreen;
 import React, { useState, useEffect } from "react";
 import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
-import { BarChart } from "react-native-chart-kit";
+import { VictoryBar, VictoryChart, VictoryTheme, VictoryAxis } from "victory-native";
 
 const FilteredDataScreen = ({ route }) => {
   const { analyticsType, filterOptionId, accessToken } = route.params;
@@ -27,7 +150,6 @@ const FilteredDataScreen = ({ route }) => {
 
         const jsonData = await response.json();
         setData(jsonData);
-        console.log(jsonData);
       } catch (error) {
         console.error("Fetch Error:", error);
         setError(error.message);
@@ -55,24 +177,10 @@ const FilteredDataScreen = ({ route }) => {
     );
   }
 
-  const chartData = {
-    labels: ["Patron Profile", "Menu Item"],
-    datasets: [
-      {
-        data: [data.number_of_patronProfile, data.number_of_menuItem],
-      },
-    ],
-  };
-
-  const chartConfig = {
-    backgroundColor: "black",
-    backgroundGradientFrom: "#392F5A",
-    backgroundGradientTo: "#ffa726",
-    decimalPlaces: 0,
-    color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-  };
-
-  console.log("ChartData:", chartData);
+  const chartData = [
+    { category: "Patron Profile", value: data.number_of_patronProfile },
+    { category: "Menu Item", value: data.number_of_menuItem },
+  ];
 
   return (
     <View style={styles.container}>
@@ -81,14 +189,17 @@ const FilteredDataScreen = ({ route }) => {
           <Text style={styles.chartTitle}>
             Number of Patron vs Num of Menu Items Containing the Tag
           </Text>
-          <BarChart
-            data={chartData}
-            width={300}
-            height={200}
-            yAxisLabel="#"
-            fromZero={true}
-            chartConfig={chartConfig}
-          />
+          <VictoryChart theme={VictoryTheme.material}>
+            <VictoryAxis />
+            <VictoryAxis dependentAxis />
+            <VictoryBar
+              data={chartData}
+              x="category"
+              y="value"
+              barRatio={0.8}
+              style={{ data: { fill: "#4CAF50" } }} 
+            />
+          </VictoryChart>
         </View>
       )}
     </View>
@@ -100,11 +211,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  },
-  screenTitle: {
-    fontSize: 20,
-    color: "#FFFFFF",
-    marginBottom: 10,
   },
   chartContainer: {
     alignItems: "center",
